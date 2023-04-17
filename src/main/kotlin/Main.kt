5a90ee74c8b1582b7eb2d5b27255279487a42a84
Rhys Davies <rhys@hadrian.co>
2023-04-17 10:10:55 -0700

    FEN String eval

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val b = Board(FENString("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R"))
+    val b = Board(INITIAL_BOARD_FEN)
     b.printState()
 
     println("\n \n MOVING \n \n")
